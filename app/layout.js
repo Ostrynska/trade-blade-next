3eb3147a2c6b5426574d3d94e9698dfa2d62f5f3
Ostrynska <kateriinag@gmail.com>
2025-03-10 16:55:08 +0200

    create project
    added layout grid

---
+++ app/layout.js
@@ -0,0 +1,26 @@
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+
+const ibmPlexSans = IBM_Plex_Sans({
+  variable: "--font-ibm-plex-sans",
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
+
+export const metadata = {
+  title: "TradeBlade",
+  description: "Моментально копируй сделки профи трейдеров",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${ibmPlexSans.variable}`}>
+        <div className="layout-wrapper">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
